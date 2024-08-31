d9a3f1abd8b360b0039dfc4f8e6ada096479f3c1
solvman <solvman@gmail.com>
2024-08-30 23:15:08 -0500

    chore: Add lazy loading for article images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,27 @@
 import { useFeaturedArticle } from "@/hooks/useFeaturedArticle";
+import Image from "next/image";
 
 function Hero() {
   const featuredArticle = useFeaturedArticle();
 
   return (
     <section>
+      {featuredArticle.img?.src.desktop && (
+        <Image
+          src={featuredArticle.img?.src.desktop}
+          alt={featuredArticle.img.alt}
+          className="hidden md:block"
+          priority
+        />
+      )}
+      {featuredArticle.img?.src.mobile && (
+        <Image
+          src={featuredArticle.img?.src.mobile}
+          alt={featuredArticle.img.alt}
+          className="block md:hidden"
+          priority
+        />
+      )}
       <h2 className="heading-xl">{featuredArticle.title}</h2>
       <p>{featuredArticle.exerpt}</p>
       <a href={featuredArticle.url}>Read more</a>
