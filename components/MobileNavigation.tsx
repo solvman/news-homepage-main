2b605e83853aa1b971537daca1c8079345a516f5
solvman <solvman@gmail.com>
2024-08-31 21:59:49 -0500

    chore: Refactor MobileNavigation component and add keyboard event listener for closing the navigation drawer

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -54,14 +54,16 @@ function MobileNavigation() {
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-8 w-8"
               >
-                <g fill="#00001A" fill-rule="evenodd">
+                <g fill="#00001A" fillRule="evenodd">
                   <path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z" />
                   <path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z" />
                 </g>
               </svg>
               <span className="sr-only">Close navigation</span>
             </button>
-            <NavigationList />
+            <nav>
+              <NavigationList />
+            </nav>
           </div>
         </div>
       </RemoveScroll>
